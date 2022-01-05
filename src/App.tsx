63bff3e7633641eb5257221c239475457df27b98
torutona <sololouve@gmail.com>
2022-01-06 06:58:54 +0900

    add function

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,14 @@ import './App.css';
 import Web3 from 'web3';
 import { AbiItem, toWei } from 'web3-utils';
 import GETContract from "./contracts/GETUSD.json";
+import Dropdown from 'react-dropdown';
+import 'react-dropdown/style.css';
 
 function App() {
-  const [walletAddress, setWalletAddress] = useState("");
-  // const [presaleldAmount, setPresaledAmount] = useState(0);
-  // const [remainAmount, setRemainAmount] = useState(0);
-  
+  const [walletAddress, setWalletAddress] = useState("");  
   const contractAddress = "0x9070B857b6fA74Dd90Aa5a3e99737F2B1d3F9Fad";
+  const [memberAddresses, setMemberAddresses] = useState<any[]>([]);
+  const [memberLevel, setMemberLevel] = useState(0);
 
   async function checkAccount() {
     let web3 = new Web3(window.ethereum);
@@ -42,94 +43,44 @@ function App() {
         GETContract as AbiItem[],
         contractAddress
       );
-      const response = await getContract.methods.name().call();
-      console.log(response);
+      console.log(memberAddresses);
+      memberAddresses.forEach(async (item: any) => {
+        const response = await getContract.methods.addInMemberList(item, memberLevel).send({from: walletAddress});
+      });
+      // const response = await getContract.methods.mint().call();
+      // console.log(response);
       // console.log(response);
     } catch(e: any) {
       console.log(e);
     }
-
   }
 
-  // const airdrop = async() => {
-  //   try {
-  //     if (window.ethereum) {
-  //       await connectWallet();
-  //     }
-  //     let web3 = new Web3(window.ethereum);
-  //     console.log(contractAddress);
-  //     const presaleContract = new web3.eth.Contract(
-  //       PresaleContract as AbiItem[],
-  //       contractAddress
-  //     );
-  //     const response: boolean = await presaleContract.methods.airdrop().send({ from: walletAddress });
-  //     console.log(response);
-  //   } catch(e: any) {
-  //     console.log(e);
-  //   }
-  // }
+  const options = [
+    { value: '0', label: 'Super'},
+    { value: '1', label: 'Special'},
+    { value: '2', label: 'Standard'},
+  ];
+  const defaultOption = options[0];
 
-  // const getPresaledAmount = async() => {
-  //   try {
-  //     let web3 = new Web3(window.ethereum);
-  //     const presaleContract = new web3.eth.Contract(
-  //       PresaleContract as AbiItem[],
-  //       contractAddress
-  //     );
-  //     const response = await presaleContract.methods.getPresaledCnt().call();
-  //     setPresaledAmount(response / 10 ** 18);
-  //   } catch(e) {
-  //     console.log(e);
-  //   }
-  // }
+  let fileReader: any;
 
-  // const getRemainPresalAmount = async() => {
-  //   try {
-  //     let web3 = new Web3(window.ethereum);
-  //     const presaleContract = new web3.eth.Contract(
-  //       PresaleContract as AbiItem[],
-  //       contractAddress
-  //     );
-  //     const response = await presaleContract.methods.getRemainPresaleCnt().call();
-  //     setRemainAmount(response / 10 ** 18);
-  //   } catch(e) { 
-  //     console.log(e);
-  //   }
-
-  // }
-
-  // const buyToken = async() => {
-  //   try {
-  //     if (window.ethereum) {
-  //       await connectWallet();
-  //     }
-  //     let web3 = new Web3(window.ethereum);
-
-  //     const presaleContract = new web3.eth.Contract(
-  //       PresaleContract as AbiItem[],
-  //       contractAddress
-  //     );
-
-  //     const amount = buyAmount; 
-  //     const price = 0.00001875 * amount;
-  //     console.log(contractAddress);
-  //     const response = await presaleContract.methods.prisale(amount).send({ 
-  //       from: walletAddress,
-  //       value: toWei(price.toString(), "ether"),
-  //       gas: 300000,
-  //     });
-  //     console.log(response);
-  //     console.log("buyToken end");
-  //   } catch(e: any) {
-  //     console.log(e);
-  //   }
-  // }
-
-  // const handleAmountChange: ChangeEventHandler<HTMLInputElement> = (e) => {
-  //   setBuyAmount(Number(e.target.value));
-  // };
-
+  const handleFileRead = (e: any) => {
+    console.log('abcd');
+    const content = fileReader.result;
+    const addresses = content.split('\n');
+    const addressList = addresses.map((address: any) => address)
+    // console.log(memberAddresses);
+    // setMemberAddresses((prevState: any[]) => [...prevState, ...addressList])
+    setMemberAddresses(addressList)
+    // … do something with the 'content' …
+  };
   
+  const handleFileChosen = (file: any) => {
+    fileReader = new FileReader();
+    fileReader.onloadend = handleFileRead;
+    fileReader.readAsText(file);
+  };
+
 
   return (
     <div className="App">
@@ -139,8 +90,27 @@ function App() {
       <button onClick={test} className='cta-button'>
         Test
       </button>
+      <Dropdown 
+        options={options} 
+        onChange={level => {
+          setMemberLevel(parseInt(level.value));
+        }}
+        value={defaultOption}
+        placeholder="Select an Level of the list" 
+      />
+      <p>{memberLevel}</p>
+      <input
+        type='file'
+        id='file'
+        className='input-file'
+        accept='.txt'
+        onChange={(e: any) => {
+            handleFileChosen(e.target.files[0])
+        }}
+      />
     </div>
   );
+
 }
 
 export default App;
